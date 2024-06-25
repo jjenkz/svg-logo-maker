19b7629d5ecb883859e822bcb0a899303177f17b
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-06-25 15:57:48 -0700

    added an array of questions for user input

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+  {
+    type: "input",
+    name: "text",
+    message: "Choose up to three characters:",
+  },
+  {
+    type: "list",
+    name: "textColor",
+    message: "Choose the text's color:",
+    choices: [
+      "red",
+      "green",
+      "yellow",
+      "blue",
+      "magenta",
+      "black",
+      "white",
+      "cyan",
+    ],
+  },
+  {
+    type: "list",
+    name: "shapes",
+    message: "Choose shapes to use:",
+    choices: ["square", "triangle", "circle"],
+  },
+  {
+    type: "list",
+    name: "shapesColor",
+    message: "Choose the shape's color:",
+    choices: [
+      "red",
+      "green",
+      "yellow",
+      "blue",
+      "magenta",
+      "black",
+      "white",
+      "cyan",
+    ],
+  },
+];
+
+console.log(questions);
