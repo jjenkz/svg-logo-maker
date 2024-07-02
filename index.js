c9b2c8b6bf64612e63b3d9f0fde967e74244453e
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-07-01 21:15:32 -0700

    nothing changed, deleted and readded code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Circle, Triangle, Square } = require("./lib/shapes.js");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 const questions = [
   {
